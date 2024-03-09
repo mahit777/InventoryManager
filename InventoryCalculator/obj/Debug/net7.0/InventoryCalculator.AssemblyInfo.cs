087a8cf12a4ace11378f7fc48c16998fb41061c4
Mahitej Gangaraju <mahitej@ncompasbusiness.com>
2024-03-09 21:43:31 +0530

    Added readme file

--- InventoryCalculator/obj/Debug/net7.0/InventoryCalculator.AssemblyInfo.cs
+++ InventoryCalculator/obj/Debug/net7.0/InventoryCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad3af56a770f8d5494f079a1e42b7a72fad1cf8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
